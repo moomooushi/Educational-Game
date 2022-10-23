f6e813f7df0763717209b46698862b1d4e1af8d6
Thomas Graham <thomasgraham874@gmail.com>
2022-10-23 14:42:21 +1100

    Added TMPro shit and practice scene. Movement added

---
+++ Assets/Scripts/PlayerOneMovement.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerOneMovement : MonoBehaviour
+{
+    private float horizontal;
+    // private bool isFacingRight = true;
+    [SerializeField] private float speed = 8f;
+    [SerializeField] private float jumpingPower = 12f;
+
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private bool isGrounded;
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+            isGrounded = false;
+        }
+
+        if (Input.GetButtonDown("Jump") && rb.velocity.y > 0f && isGrounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+        }
+
+        //Flip();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.gameObject.layer == 6)
+        {
+            isGrounded = true;
+        }
+    }
+
+    //private void Flip()
+    //{
+    //    if (isFacingRight && horizontal < 0 || !isFacingRight && horizontal > 0)
+    //    {
+    //        isFacingRight = !isFacingRight;
+    //        Vector3 localScale = transform.localScale;
+    //        localScale.x *= -1f;
+    //        transform.localScale = localScale;
+    //    }
+    //}
+}
